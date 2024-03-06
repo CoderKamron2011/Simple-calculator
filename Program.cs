d19b347f6b24ddefede96c37b386a36dc34aaeca
CoderKamron2011 <hasanovaruxshona774@gmail.com>
2024-03-06 17:31:56 +0200

    codes done

--- Program.cs
+++ Program.cs
@@ -1,1 +1,30 @@
-﻿Console.ReadLine("Enter first number: ");
+﻿
+
+int number ,number_2;
+System.Console.Write("Enter first number: ");
+number = int.Parse(Console.ReadLine());
+System.Console.Write("Enter first number: ");
+number_2 = int.Parse(Console.ReadLine());
+string amal;
+System.Console.Write("amalni kiriting: ");
+amal = Console.ReadLine();
+number = ((int)number);
+
+//business logic
+if (amal == "+")
+{
+    System.Console.WriteLine(number + number_2);
+}
+else if (amal == "-")
+{
+    System.Console.WriteLine(number - number_2);
+}
+else if (amal == "*")
+{
+    System.Console.WriteLine(number * number_2);
+}
+else if (amal == "/")
+{
+    System.Console.WriteLine(number / number_2);
+}
+
